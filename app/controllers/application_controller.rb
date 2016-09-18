098c2ff5e2b0747c8a026dadc61403bddbbf11b2
PC Apple1 <pc_apple1@opencart.com>
2016-09-18 22:09:33 +0700

    create function dump

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def sd(var_dump)
+  	render html: var_dump.inspect
+  end
+
+  def d(var_dump)
+  	return var_dump.inspect
+  end
 end
